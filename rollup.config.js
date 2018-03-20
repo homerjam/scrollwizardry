bb607ef4ca395bbb7f0f54a61e3649fbfeb5c757
James Homer <jameshomer85@gmail.com>
2018-03-20 12:37:30 +0000

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,42 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
+import pkg from './package.json';
 
-export default {
-  input: 'src/index.js',
-  output: {
-    file: 'dist/scrollwizardry.js',
-    format: 'umd',
+export default [
+  // browser-friendly UMD build
+  {
+    input: 'src/index.js',
+    output: {
+      name: 'ScrollWizardry',
+      file: pkg.browser,
+      format: 'umd',
+      sourcemap: true,
+      globals: {
+        lodash: '_',
+      },
+    },
+    plugins: [
+      resolve(), // so Rollup can find `lodash`
+      commonjs(), // so Rollup can convert `lodash` to an ES module
+      babel({
+        exclude: 'node_modules/**',
+      }),
+    ],
   },
-  name: 'ScrollWizardry',
-  sourcemap: true,
-  plugins: [
-    babel({
-      exclude: 'node_modules/**',
-    }),
-  ],
-  external: ['lodash'],
-  globals: {
-    lodash: '_',
+
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  // (We could have three entries in the configuration array
+  // instead of two, but it's quicker to generate multiple
+  // builds from a single configuration where possible, using
+  // an array for the `output` option, where we can specify
+  // `file` and `format` for each target)
+  {
+    input: 'src/index.js',
+    external: ['lodash'],
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' },
+    ],
   },
-};
+];
